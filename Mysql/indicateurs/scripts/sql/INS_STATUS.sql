83e1d4ca54b1e6ae4f6f3645393b3049da934c37
memodba <memodba@gmail.com>
2014-01-27 15:49:00 +0100

    Ajout indicateurs Sonar

--- Mysql/indicateurs/scripts/sql/INS_STATUS.sql
+++
@@ -1,7 +0,0 @@
-BEGIN 
-select table_schema,table_name,count(column_name) as Nb from 
-INFORMATION_SCHEMA.COLUMNS
-group by table_schema,table_name
-having count(column_name) >=30
-order by nb desc;
-END ;
--- Mysql/indicateurs/scripts/sql/INS_STATUS_PG.sql
+++
@@ -1,5 +0,0 @@
-select table_schema,table_name,count(column_name) as Nb from 
-INFORMATION_SCHEMA.COLUMNS
-group by table_schema,table_name
-having count(column_name) >=30
-order by nb desc;
